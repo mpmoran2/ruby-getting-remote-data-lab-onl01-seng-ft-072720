31225b07d58bfdb78b070d1ff63586d118139b03
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-12 20:49:37 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,18 @@ class GetRequester
     @url = url
   end 
   
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body 
+  end 
   
+  def parse_json
+    programs = JSON.parse(self.get_response_body)
+    programs.collect do |program|
+      program
+    end
+  end 
 
 end 
 
